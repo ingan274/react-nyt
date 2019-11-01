bc829586544dc36038c29d254f77eedc93f9f405
ingan <isabelngan27@gmail.com>
2019-11-01 15:38:29 -0700

    adding build

---
+++ client/build/precache-manifest.cb342f83db09a99114e972e27735b709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ea39c583f6b58b1db6592f94aca1c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b183178a24a054f14ca0",
+    "url": "/static/css/main.c22c4aa6.chunk.css"
+  },
+  {
+    "revision": "aff3ac10b15afd4dcae2",
+    "url": "/static/js/2.34790fe9.chunk.js"
+  },
+  {
+    "revision": "b183178a24a054f14ca0",
+    "url": "/static/js/main.6d642f64.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  }
+]);

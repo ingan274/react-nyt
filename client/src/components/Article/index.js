67fa8d2c6be1ac7de8d79d08e25a42af7eff4f23
ingan <isabelngan27@gmail.com>
2019-11-02 10:38:57 -0700

    adding font file

--- client/src/components/Article/index.js
+++ client/src/components/Article/index.js
@@ -4,6 +4,7 @@ import "./style.css"
 const Article = props => (
     <li className="list-group-item">
         <p className="articleTitle"><a href={props.url} rel="noopener noreferrer" target="_blank">{props.title}</a></p>
+        <p className="articleAuthor">by {props.author}</p>
         <p className="articleDesc">{props.description}</p>
         <button className="btn" onClick={() => props.handleClick(props._id)}>
             {props.buttonText}
--- models/Article.js
+++ models/Article.js
@@ -6,7 +6,7 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  link: {
+  url: {
     type: String,
     required: true
   },
